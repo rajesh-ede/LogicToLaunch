be6ebf1bc2db4fac37f1e7b5919a144f250f8750
rajesh-ede <rajeshede208@gmail.com>
2026-08-25 23:49:10 +0530

    Task Updated

---
+++ Institute/levelUpLog/August_2026/Aug_25/Pg3.java
@@ -0,0 +1,53 @@
+class Pg3
+{
+    static byte bb = 1;
+    static short ss = 38;
+    static int ii = 7184;
+    static long ll = 9765432;
+    static float ff = 2.5f;
+    static double dd = 100.00;
+    static  String ss = "Cv_Crop";
+
+      byte bbb = 1;
+      short sss = 38;
+      int iii = 7184;
+      long lll = 9765432;
+      float fff = 2.5f;
+      double ddd = 100.00;
+      String sss = "Java if Fun";
+
+   public static void main(String[] args)
+   {
+     System.out.println("Local Variables");
+     byte b = 1;
+     short s = 38;
+     int i = 7184;
+     long l = 9765432;
+     float f = 2.5f;
+     double d = 100.00;
+     String s = "Java if Fun";
+     System.out.println(b);
+     System.out.println(s);
+     System.out.println(i);
+     System.out.println(l);
+     System.out.println(f);
+     System.out.println(d);
+     System.out.println(s);
+     System.out.println("Main method was Executed Successfully");
+
+     System.out.println("Accessing static varibales");
+     Sytsem.out.println(bb);
+     Sytsem.out.println(ss);
+     Sytsem.out.println(ii);
+     Sytsem.out.println(ll);
+     Sytsem.out.println(ff);
+     Sytsem.out.println(dd);
+     Sytsem.out.println(ss);
+    
+    Pg3 obj = new Pg3();
+    
+    }
+}
+    
+  
+     
